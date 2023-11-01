53ec02335549fc11e36a9e224fb7f38378c6d196
HanYewei <HanYewei@yx.com>
2023-11-01 19:12:34 +0800

    create 01

---
+++ src/main/java/com/atguigu/Test.java
@@ -0,0 +1,18 @@
+package com.atguigu;
+
+/**
+ * ClassName: Test
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author 韩业伟
+ * @Create 2023/11/1 19:08
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
