9762e10f522e41fe924fd14676c49fea84b39cc0
HanYewei <HanYewei@yx.com>
2023-11-01 19:21:33 +0800

    master 02

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -12,7 +12,7 @@ package com.atguigu;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello aaa World!");
         System.out.println("dev!!!");
 
     }
